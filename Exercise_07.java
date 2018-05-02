e3bb4e1928a53f9082e1a673e855d21f999b782b
SebastianDuedahl <33715468+SebastianDuedahl@users.noreply.github.com>
2018-05-02 16:53:27 +0200

    Add files via upload

---
+++ Exercise_07.java
@@ -0,0 +1,18 @@
+package Chapter_11_a_07;
+
+/*
+Write a method countCommon that accepts two lists of integers as parameters and returns the number of unique integers
+that occur in both lists. Use one or more sets as storage to help you solve this problem. For example, if one list contains
+the values [3, 7, 3, –1, 2, 3, 7, 2, 15, 15] and the other list contains the values [–5, 15, 2, –1, 7,
+15, 36], your method should return 4 because the elements –1, 2, 7, and 15 occur in both lists
+ */
+
+public class Exercise_07
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+
+}
